722b7e15079182e376c13a1f11c2c55e9a1edf4e
kie-koutsuka <kiekobo@gmail.com>
2025-08-10 09:22:14 +0900

    変更

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,40 @@
+<script setup>
+import { computed, ref } from 'vue'
+
+const addText = ref('') //新規追加TODO
+//const completion = ref(false) //完了(true) /未完了(false)※デフォルトfalse
+
+let id = 0 //TODOのID(内部用)
+const listDatas = ref([]) //TODOリスト表示用配列
+
+//↓↓サマリ結果表示用const//
+//TODO件数ー全件
+const summaryAll = computed(() => listDatas.value.length)
+
+//TODO件数ー完了
+const summaryCompletion = computed(() => {
+  const list = listDatas.value.filter((t) => t.Completion === true)
+  return list.length
+})
+
+//TODO件数ー未完了
+const summaryNotCompletion = computed(() => {
+  const list = listDatas.value.filter((t) => t.Completion === false)
+  return list.length
+})
+
+function listAddNewTodo() {
+  //TODOリストにアイテム追加
+  listDatas.value.push({ id: id++, text: addText.value, Completion: false })
+  addText.value = ''
+}
+
+function listDelTodo(todo) {
+  //TODOリストからアイテム削除
+  listDatas.value = listDatas.value.filter((t) => t !== todo)
+}
+</script>
+
 <template>
   <header id="header-area">
     <h1>TODOメモ</h1>
@@ -5,20 +42,28 @@
 
   <main id="main-area">
     <div id="add-area">
-      <input type="text" id="add-text" value="新しいタスクを追加" /><button>追加</button>
+      <input v-model="addText" type="text" id="add-text" placeholder="新しいタスクを追加" />
+      <button @click="listAddNewTodo">追加</button>
     </div>
 
     <div id="list-area">
+      <!-- TODOmemoヘッダ -->
       <div id="list-header">
         <div class="completion-checkbox">完了</div>
         <div class="task">タスク名</div>
         <div class="button"></div>
       </div>
-      <div class="list-detail">
-        <div class="completion-checkbox"><input class="checkbox" type="checkbox" /></div>
-        <div class="task">AAAAAA</div>
-        <div class="button"><button>編集</button><button>削除</button></div>
+      <!-- TODOmemo明細 -->
+      <div v-for="listData in listDatas" :key="listData.id" class="list-detail">
+        <div class="completion-checkbox">
+          <input class="checkbox" type="checkbox" />
+        </div>
+        <div class="task">{{ listData.text }}</div>
+        <div class="button">
+          <button>編集</button><button @click="listDelTodo(listData)">削除</button>
+        </div>
       </div>
+      <!--
       <div class="list-detail">
         <div class="completion-checkbox"><input class="checkbox" type="checkbox" /></div>
         <div class="task">BBBBBBBBBBBBBBBB</div>
@@ -39,7 +84,14 @@
         <div class="task">他の行を編集中は使用不可</div>
         <div class="button"><button disabled>編集</button><button disabled>削除</button></div>
       </div>
-      <p id="summary-area">全<span>2</span>件 / 完了<span>1</span>件 / 残<span>1</span>件</p>
+      CSS確認用おわり-->
+      <!-- TODOmemoサマリ-->
+      <p id="summary-area">
+        全<span>{{ summaryAll }}</span
+        >件 / 完了<span>{{ summaryCompletion }}</span
+        >件 / 残<span>{{ summaryNotCompletion }}</span
+        >件
+      </p>
     </div>
 
     <div id="switching-area">
@@ -48,7 +100,3 @@
     </div>
   </main>
 </template>
-
-<script setup></script>
-
-<style scoped></style>
